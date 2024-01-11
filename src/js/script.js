c7c2f2e9db2a71743bb647e14f681dc1bcd080da
Gabriel Augusto <ga_consalter@hotmail.com>
2024-01-11 14:46:29 -0300

    Update Project Container

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,25 @@
+const projetos = document.querySelectorAll('.position-container')
 const nextButton = document.querySelector('.button-next')
-const backButton = document.querySelector('.button-back')
+const backButton = document.querySelector('.button-previous')
 
-nextButton.addEventListener('click', loadNextPage)
-
-// backButton.addEventListener('click', laodPreviusPage)
-
-function loadNextPage() {
-    const projetos = document.querySelectorAll('.position-container')
-    const projetoSelecionado = document.querySelectorAll('.position-container.selecionado')
-
-    projetos.forEach((projeto) =>{
-        console.log(projeto);
-     })
-
-    projetoSelecionado.forEach(projeto => {
-        projeto.classList.remove('selecionado')
+projetos.forEach((p, i) => {
+    nextButton.addEventListener('click', () => {
+        if(i == 0) {
+            projetos[i+2].classList.add('selecionado')
+            projetos[i+3].classList.add('selecionado')
+            projetos[i].classList.remove('selecionado')
+            projetos[i+1].classList.remove('selecionado')
+        }
     })
 
-}
+    backButton.addEventListener('click', () => {
+        console.log(i);
+        if(i == 0){
+            projetos[i].classList.add('selecionado')
+            projetos[i+1].classList.add('selecionado')
+            projetos[i+2].classList.remove('selecionado')
+            projetos[i+3].classList.remove('selecionado')
+        }
+    })
+})
+
